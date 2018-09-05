798218db516343078e828441784832b65c8fa809
Anwesh43 <anweshthecool0@gmail.com>
2018-09-05 10:52:12 +0530

    adding CenterBallStage class

---
+++ CenterBallStage.ts
@@ -0,0 +1,33 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+class CenterBallStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    constructor() {
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : CenterBallStage = new CenterBallStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
