101807b9a23afd5821862f7e06fb52664586eb85
Anwesh43 <anweshthecool0@gmail.com>
2018-09-05 11:22:27 +0530

    adding LinkedCenterBall class

--- CenterBallStage.ts
+++ CenterBallStage.ts
@@ -130,3 +130,27 @@ class CBNode {
         context.restore()
     }
 }
+
+class LinkedCenterBall {
+    root : CBNode = new CBNode(0)
+    curr : CBNode = this.root
+    dir : number = 1
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+
+
+}
